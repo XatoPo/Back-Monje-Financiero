b200ca4d48a613b838e8f50e9db5807e3637ce0a
XatoPo <flaviovm2013@gmail.com>
2024-10-06 23:24:45 -0500

    "Updated database schema and API endpoints: changed ID column names in several tables, added new API endpoint /usersTest, and modified existing /ping endpoint."

--- db/dbMonjeFinanciero.sql
+++ db/dbMonjeFinanciero.sql
@@ -15,16 +15,16 @@ CREATE TABLE Users (
 );
 
 INSERT INTO Users (id, name, email, password, date_of_birth, profile_image_url) VALUES
-('1', 'Flavio Villanueva', 'flavio@example.com', 'hashedpassword1', '1990-01-01', 'https://example.com/image1.jpg'),
-('2', 'Carlos Perez', 'carlos@example.com', 'hashedpassword2', '1992-02-02', 'https://example.com/image2.jpg'),
-('3', 'Maria Lopez', 'maria@example.com', 'hashedpassword3', '1993-03-03', 'https://example.com/image3.jpg'),
-('4', 'Ana Garcia', 'ana@example.com', 'hashedpassword4', '1988-04-04', 'https://example.com/image4.jpg'),
-('5', 'Jose Martinez', 'jose@example.com', 'hashedpassword5', '1985-05-05', 'https://example.com/image5.jpg'),
-('6', 'Laura Sanchez', 'laura@example.com', 'hashedpassword6', '1991-06-06', 'https://example.com/image6.jpg'),
-('7', 'Ricardo Torres', 'ricardo@example.com', 'hashedpassword7', '1987-07-07', 'https://example.com/image7.jpg'),
-('8', 'Patricia Jimenez', 'patricia@example.com', 'hashedpassword8', '1994-08-08', 'https://example.com/image8.jpg'),
-('9', 'Luis Ramirez', 'luis@example.com', 'hashedpassword9', '1995-09-09', 'https://example.com/image9.jpg'),
-('10', 'Sofia Gonzalez', 'sofia@example.com', 'hashedpassword10', '1996-10-10', 'https://example.com/image10.jpg');
+('US001', 'Flavio Villanueva', 'flavio@example.com', 'hashedpassword1', '1990-01-01', 'https://example.com/image1.jpg'),
+('US002', 'Carlos Perez', 'carlos@example.com', 'hashedpassword2', '1992-02-02', 'https://example.com/image2.jpg'),
+('US003', 'Maria Lopez', 'maria@example.com', 'hashedpassword3', '1993-03-03', 'https://example.com/image3.jpg'),
+('US004', 'Ana Garcia', 'ana@example.com', 'hashedpassword4', '1988-04-04', 'https://example.com/image4.jpg'),
+('US005', 'Jose Martinez', 'jose@example.com', 'hashedpassword5', '1985-05-05', 'https://example.com/image5.jpg'),
+('US006', 'Laura Sanchez', 'laura@example.com', 'hashedpassword6', '1991-06-06', 'https://example.com/image6.jpg'),
+('US007', 'Ricardo Torres', 'ricardo@example.com', 'hashedpassword7', '1987-07-07', 'https://example.com/image7.jpg'),
+('US008', 'Patricia Jimenez', 'patricia@example.com', 'hashedpassword8', '1994-08-08', 'https://example.com/image8.jpg'),
+('US009', 'Luis Ramirez', 'luis@example.com', 'hashedpassword9', '1995-09-09', 'https://example.com/image9.jpg'),
+('US010', 'Sofia Gonzalez', 'sofia@example.com', 'hashedpassword10', '1996-10-10', 'https://example.com/image10.jpg');
 
 -- Crear tabla Categories
 CREATE TABLE Categories (
@@ -36,16 +36,16 @@ CREATE TABLE Categories (
 );
 
 INSERT INTO Categories (id, user_id, name, color, icon_url) VALUES
-('1', '1', 'Alimentación', '#FF5733', 'https://example.com/icon1.png'),
-('2', '1', 'Transporte', '#33FF57', 'https://example.com/icon2.png'),
-('3', '1', 'Entretenimiento', '#3357FF', 'https://example.com/icon3.png'),
-('4', '1', 'Salud', '#F5FF33', 'https://example.com/icon4.png'),
-('5', '1', 'Hogar', '#FF33B5', 'https://example.com/icon5.png'),
-('6', '1', 'Educación', '#33FFED', 'https://example.com/icon6.png'),
-('7', '1', 'Compras', '#B533FF', 'https://example.com/icon7.png'),
-('8', '1', 'Trabajo', '#FF33A1', 'https://example.com/icon8.png'),
-('9', '1', 'Viajes', '#FFC300', 'https://example.com/icon9.png'),
-('10', '1', 'Tecnología', '#C70039', 'https://example.com/icon10.png');
+('CT001', 'US001', 'Alimentación', '#FF5733', 'https://example.com/icon1.png'),
+('CT002', 'US001', 'Transporte', '#33FF57', 'https://example.com/icon2.png'),
+('CT003', 'US001', 'Entretenimiento', '#3357FF', 'https://example.com/icon3.png'),
+('CT004', 'US001', 'Salud', '#F5FF33', 'https://example.com/icon4.png'),
+('CT005', 'US001', 'Hogar', '#FF33B5', 'https://example.com/icon5.png'),
+('CT006', 'US001', 'Educación', '#33FFED', 'https://example.com/icon6.png'),
+('CT007', 'US001', 'Compras', '#B533FF', 'https://example.com/icon7.png'),
+('CT008', 'US001', 'Trabajo', '#FF33A1', 'https://example.com/icon8.png'),
+('CT009', 'US001', 'Viajes', '#FFC300', 'https://example.com/icon9.png'),
+('CT010', 'US001', 'Tecnología', '#C70039', 'https://example.com/icon10.png');
 
 -- Crear tabla Expenses
 CREATE TABLE Expenses (
@@ -61,16 +61,16 @@ CREATE TABLE Expenses (
 );
 
 INSERT INTO Expenses (id, user_id, description, amount, category_id, date, is_recurring) VALUES
-('1', '1', 'Compra de frutas', 20.00, '1', '2024-01-15', FALSE),
-('2', '1', 'Pasajes de bus', 5.00, '2', '2024-01-16', TRUE),
-('3', '1', 'Cine con amigos', 15.00, '3', '2024-01-17', FALSE),
-('4', '1', 'Medicinas', 30.00, '4', '2024-01-18', FALSE),
-('5', '1', 'Compra de muebles', 200.00, '5', '2024-01-19', FALSE),
-('6', '1', 'Libros de texto', 50.00, '6', '2024-01-20', FALSE),
-('7', '1', 'Ropa nueva', 100.00, '7', '2024-01-21', TRUE),
-('8', '1', 'Gastos de oficina', 75.00, '8', '2024-01-22', FALSE),
-('9', '1', 'Hotel durante el viaje', 300.00, '9', '2024-01-23', FALSE),
-('10', '1', 'Nuevo ordenador', 800.00, '10', '2024-01-24', FALSE);
+('EP001', 'US001', 'Compra de frutas', 20.00, 'CT001', '2024-01-15', FALSE),
+('EP002', 'US001', 'Pasajes de bus', 5.00, 'CT002', '2024-01-16', TRUE),
+('EP003', 'US001', 'Cine con amigos', 15.00, 'CT003', '2024-01-17', FALSE),
+('EP004', 'US001', 'Medicinas', 30.00, 'CT004', '2024-01-18', FALSE),
+('EP005', 'US001', 'Compra de muebles', 200.00, 'CT005', '2024-01-19', FALSE),
+('EP006', 'US001', 'Libros de texto', 50.00, 'CT006', '2024-01-20', FALSE),
+('EP007', 'US001', 'Ropa nueva', 100.00, 'CT007', '2024-01-21', TRUE),
+('EP008', 'US001', 'Gastos de oficina', 75.00, 'CT008', '2024-01-22', FALSE),
+('EP009', 'US001', 'Hotel durante el viaje', 300.00, 'CT009', '2024-01-23', FALSE),
+('EP010', 'US001', 'Nuevo ordenador', 800.00, 'CT010', '2024-01-24', FALSE);
 
 -- Crear tabla Budgets
 CREATE TABLE Budgets (
@@ -85,16 +85,16 @@ CREATE TABLE Budgets (
 );
 
 INSERT INTO Budgets (id, user_id, name, budget_limit, category_id, period) VALUES
-('1', '1', 'Ahorros Mensuales', 500.00, '1', 'monthly'),
-('2', '1', 'Gastos de Comida', 300.00, '2', 'monthly'),
-('3', '1', 'Viajes', 1500.00, '9', 'yearly'),
-('4', '1', 'Educación', 600.00, '6', 'monthly'),
-('5', '1', 'Entretenimiento', 200.00, '3', 'monthly'),
-('6', '1', 'Salud', 250.00, '4', 'monthly'),
-('7', '1', 'Hogar', 800.00, '5', 'monthly'),
-('8', '1', 'Tecnología', 1200.00, '10', 'yearly'),
-('9', '1', 'Compras', 1000.00, '7', 'yearly'),
-('10', '1', 'Gastos Varios', 400.00, '8', 'monthly');
+('BU001', 'US001', 'Ahorros Mensuales', 500.00, 'US001', 'monthly'),
+('BU002', 'US001', 'Gastos de Comida', 300.00, '2', 'monthly'),
+('BU003', 'US001', 'Viajes', 1500.00, '9', 'yearly'),
+('BU004', 'US001', 'Educación', 600.00, '6', 'monthly'),
+('BU005', 'US001', 'Entretenimiento', 200.00, '3', 'monthly'),
+('BU006', 'US001', 'Salud', 250.00, '4', 'monthly'),
+('BU007', 'US001', 'Hogar', 800.00, '5', 'monthly'),
+('BU008', 'US001', 'Tecnología', 1200.00, '10', 'yearly'),
+('BU009', 'US001', 'Compras', 1000.00, '7', 'yearly'),
+('BU010', 'US001', 'Gastos Varios', 400.00, '8', 'monthly');
 
 -- Crear tabla Reports
 CREATE TABLE Reports (
@@ -108,16 +108,16 @@ CREATE TABLE Reports (
 );
 
 INSERT INTO Reports (id, user_id, start_date, end_date, total_expenses, category_breakdown) VALUES
-('1', '1', '2024-01-01', '2024-01-31', 500.00, '{"Alimentación": 200, "Transporte": 100, "Entretenimiento": 200}'),
-('2', '1', '2024-02-01', '2024-02-29', 400.00, '{"Alimentación": 150, "Transporte": 150, "Entretenimiento": 100}'),
-('3', '1', '2024-03-01', '2024-03-31', 800.00, '{"Alimentación": 300, "Transporte": 200, "Entretenimiento": 300}'),
-('4', '1', '2024-04-01', '2024-04-30', 600.00, '{"Alimentación": 250, "Transporte": 200, "Entretenimiento": 150}'),
-('5', '1', '2024-05-01', '2024-05-31', 700.00, '{"Alimentación": 300, "Transporte": 200, "Entretenimiento": 200}'),
-('6', '1', '2024-06-01', '2024-06-30', 900.00, '{"Alimentación": 400, "Transporte": 300, "Entretenimiento": 200}'),
-('7', '1', '2024-07-01', '2024-07-31', 300.00, '{"Alimentación": 100, "Transporte": 100, "Entretenimiento": 100}'),
-('8', '1', '2024-08-01', '2024-08-31', 850.00, '{"Alimentación": 300, "Transporte": 250, "Entretenimiento": 300}'),
-('9', '1', '2024-09-01', '2024-09-30', 700.00, '{"Alimentación": 350, "Transporte": 200, "Entretenimiento": 150}'),
-('10', '1', '2024-10-01', '2024-10-31', 950.00, '{"Alimentación": 450, "Transporte": 350, "Entretenimiento": 150}');
+('RP001', 'US001', '2024-01-01', '2024-01-31', 500.00, '{"Alimentación": 200, "Transporte": 100, "Entretenimiento": 200}'),
+('RP002', 'US001', '2024-02-01', '2024-02-29', 400.00, '{"Alimentación": 150, "Transporte": 150, "Entretenimiento": 100}'),
+('RP003', 'US001', '2024-03-01', '2024-03-31', 800.00, '{"Alimentación": 300, "Transporte": 200, "Entretenimiento": 300}'),
+('RP004', 'US001', '2024-04-01', '2024-04-30', 600.00, '{"Alimentación": 250, "Transporte": 200, "Entretenimiento": 150}'),
+('RP005', 'US001', '2024-05-01', '2024-05-31', 700.00, '{"Alimentación": 300, "Transporte": 200, "Entretenimiento": 200}'),
+('RP006', 'US001', '2024-06-01', '2024-06-30', 900.00, '{"Alimentación": 400, "Transporte": 300, "Entretenimiento": 200}'),
+('RP007', 'US001', '2024-07-01', '2024-07-31', 300.00, '{"Alimentación": 100, "Transporte": 100, "Entretenimiento": 100}'),
+('RP008', 'US001', '2024-08-01', '2024-08-31', 850.00, '{"Alimentación": 300, "Transporte": 250, "Entretenimiento": 300}'),
+('RP009', 'US001', '2024-09-01', '2024-09-30', 700.00, '{"Alimentación": 350, "Transporte": 200, "Entretenimiento": 150}'),
+('RP010', 'US001', '2024-10-01', '2024-10-31', 950.00, '{"Alimentación": 450, "Transporte": 350, "Entretenimiento": 150}');
 
 -- Crear tabla Meta
 CREATE TABLE Meta (
@@ -130,13 +130,13 @@ CREATE TABLE Meta (
 );
 
 INSERT INTO Meta (id, user_id, target_amount, achieved_amount, deadline) VALUES
-('1', '1', 1000.00, 500.00, '2024-12-31'),
-('2', '1', 1500.00, 750.00, '2025-01-31'),
-('3', '1', 2000.00, 1000.00, '2025-02-28'),
-('4', '1', 3000.00, 1500.00, '2025-03-31'),
-('5', '1', 2500.00, 1200.00, '2025-04-30'),
-('6', '1', 1800.00, 800.00, '2025-05-31'),
-('7', '1', 2200.00, 1100.00, '2025-06-30'),
-('8', '1', 2700.00, 1350.00, '2025-07-31'),
-('9', '1', 3000.00, 1500.00, '2025-08-31'),
-('10', '1', 3200.00, 1600.00, '2025-09-30');
+('MT001', 'US001', 1000.00, 500.00, '2024-12-31'),
+('MT002', 'US001', 1500.00, 750.00, '2025-01-31'),
+('MT003', 'US001', 2000.00, 1000.00, '2025-02-28'),
+('MT004', 'US001', 3000.00, 1500.00, '2025-03-31'),
+('MT005', 'US001', 2500.00, 1200.00, '2025-04-30'),
+('MT006', 'US001', 1800.00, 800.00, '2025-05-31'),
+('MT007', 'US001', 2200.00, 1100.00, '2025-06-30'),
+('MT008', 'US001', 2700.00, 1350.00, '2025-07-31'),
+('MT009', 'US001', 3000.00, 1500.00, '2025-08-31'),
+('MT010', 'US001', 3200.00, 1600.00, '2025-09-30');
--- src/app.js
+++ src/app.js
@@ -7,11 +7,21 @@ app.get("/", (req, res) => {
     res.send("Monje Financiero");
 });
 
-app.get("/ping", (req, res) => {
-    const result = pool.query("SELECT id, name, email, password, date_of_birth, profile_image_url FROM users");
-    res.json(result.rows);
-    res.send("Monje Financiero - Users");
+app.get("/ping", async (req, res) => {
+    const [result] = await pool.query(`SELECT "hello world" as RESULT`);
+    console.log(result);
+    //res.send("Monje Financiero - Ping");
+    res.json(result);
 });
+
+app.get("/usersTest", async (req, res) => {
+    const [result] = await pool.query("SELECT id, name, email, password, date_of_birth, profile_image_url FROM users");
+    console.log(result);
+    //res.send("Monje Financiero - Users");
+    res.json(result);
+});
+
+app.post
 
 app.listen(3000, () => {
     console.log("Servidor escuchando en el puerto 3000");
