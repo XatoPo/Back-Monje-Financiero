03f3131073f9476e862768ef07d8550765f1f14c
XatoPo <flaviovm2013@gmail.com>
2024-10-07 12:15:38 -0500

    "Removed dotenv import and config, updated database password, and reformatted config exports"

--- src/config.js
+++ src/config.js
@@ -1,12 +1,6 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || 'junction.proxy.rlwy.net';
-export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'default_password'; // Cambia esto por un valor seguro
-export const DB_NAME = process.env.DB_NAME || 'railway';
-export const DB_PORT = process.env.DB_PORT || 15082;
-
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
+export const PORT = process.env.PORT || 3000
+export const DB_HOST = process.env.DB_HOST || 'junction.proxy.rlwy.net'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'pjsZnbDcqmReUOEUlLLzKGxrZdOxSrgP'
+export const DB_NAME = process.env.DB_NAME || 'railway'
+export const DB_PORT = process.env.DB_PORT || 15082
