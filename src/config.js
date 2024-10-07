12b936118fc4a5787c21109c563ec6e979201183
XatoPo <flaviovm2013@gmail.com>
2024-10-07 11:53:48 -0500

    "Update DB_HOST and DB_PORT in config.js"

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 3000
-export const DB_HOST = process.env.DB_HOST || 'mysql.railway.internal'
+export const DB_HOST = process.env.DB_HOST || 'junction.proxy.rlwy.net'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'pjsZnbDcqmReUOEUlLLzKGxrZdOxSrgP'
 export const DB_NAME = process.env.DB_NAME || 'railway'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_PORT = process.env.DB_PORT || 15082
