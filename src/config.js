58660e49e23c4bd07c2f8a419acf8fe4c7e6ce64
XatoPo <flaviovm2013@gmail.com>
2024-10-07 12:27:49 -0500

    "Updated config.js: removed default DB password, added console logs for DB host and user, and added newline at end of file"

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 export const PORT = process.env.PORT || 3000
 export const DB_HOST = process.env.DB_HOST || 'junction.proxy.rlwy.net'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'pjsZnbDcqmReUOEUlLLzKGxrZdOxSrgP'
+export const DB_PASSWORD = process.env.DB_PASSWORD || ''
 export const DB_NAME = process.env.DB_NAME || 'railway'
 export const DB_PORT = process.env.DB_PORT || 15082
+
+console.log('DB_HOST', process.env.DB_HOST);
+console.log('DB_USER', process.env.DB_USER);
